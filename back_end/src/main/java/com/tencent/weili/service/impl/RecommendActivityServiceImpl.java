1a1805b0d15f00fde1bdc7f46cc09a828bb41ce4
aesopliang(梁铭霏) <aesopliang@tencent.com>
2018-05-02 00:06:46 +0800

    update function GetIdByTime

--- back_end/src/main/java/com/tencent/weili/service/impl/RecommendActivityServiceImpl.java
+++ back_end/src/main/java/com/tencent/weili/service/impl/RecommendActivityServiceImpl.java
@@ -8,6 +8,6 @@ public class RecommendActivityServiceImpl {
     //时间传输 yyyyMMdd HH:MM:ss 暂时先对时间求余  之后修改
     public Integer GetIdByTime(String time) {
         String yyyyMMdd = time.split(" ")[0];
-        return parseInt(yyyyMMdd) % 10;
+        return parseInt(yyyyMMdd) % 10 + 1;
     }
 }
