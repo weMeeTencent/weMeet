bde29d411906cb34f9f2167a501d2b2bd78bfe30
wuyouxiong <wyouxiong@gmail.com>
2018-05-05 22:45:50 +0800

    初步完成推荐和发表活动页面布局

--- front_end/pages/recommend/index.js
+++ front_end/pages/recommend/index.js
@@ -5,7 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    list: [
+      {
+        id: '1',
+        img: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1525091073&di=3a5535861c7b993bb7bd0615c7eac3a2&src=http://pic.qiantucdn.com/00/91/59/42bOOOPIC98.jpg',
+        title: '春末夏初，组队攀岩了解一下',
+        desc: '负重向上，始终下坠，都是生活常态',
+        loc: '和平里~兰唐国际极限运动 5.4公里',
+        num: '2-6人',
+      },
+      {
+        id: '1',
+        img: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1525091073&di=3a5535861c7b993bb7bd0615c7eac3a2&src=http://pic.qiantucdn.com/00/91/59/42bOOOPIC98.jpg',
+        title: '春末夏初，组队攀岩了解一下',
+        desc: '负重向上，始终下坠，都是生活常态',
+        loc: '和平里~兰唐国际极限运动 5.4公里',
+        num: '2-6人',
+      }, {
+        id: '1',
+        img: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1525091073&di=3a5535861c7b993bb7bd0615c7eac3a2&src=http://pic.qiantucdn.com/00/91/59/42bOOOPIC98.jpg',
+        title: '春末夏初，组队攀岩了解一下',
+        desc: '负重向上，始终下坠，都是生活常态',
+        loc: '和平里~兰唐国际极限运动 5.4公里',
+        num: '2-6人',
+      }
+    ]
   },
 
   /**
@@ -62,5 +86,19 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  
+  kindToggle: function (e) {
+    var id = e.currentTarget.id, list = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list: list
+    });
   }
 })
