0afc41b1668084ccb768cccecd20c35afb0d6b20
erxiao <827574297wxxtb@sina.com>
2018-05-07 19:12:24 +0800

    add week

--- front_end/pages/main/calendar/index.js
+++ front_end/pages/main/calendar/index.js
@@ -1,26 +1,9 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // // // // // // // // // // // // // // 
 //
 //            日历相关函数
 //
 // // // // // // // // // // // // // // 
 
-var ccFile = require('../../../utils/calendar-converter.js')
-var calendarConverter = new ccFile.CalendarConverter();
 var app = getApp();
 
 //月份天数表
@@ -55,30 +38,21 @@ var getOffDays = function (startDate, endDate) {
   return (mmSec / 3600000 / 24);
 };
 
+//获取可选时间段
+var getSelectable = function (startDate, endDate) {
+  //得到时间戳相减 得到以毫秒为单位的差  
+  var mmSec = (endDate.getTime() - startDate.getTime());
+  //单位转换为天并返回 
+  return (mmSec / 3600000 / 24);
+};
+
 var pageData = {
   dateData: {
     date: "",                //当前日期字符串
-    arrIsShow: [],           //是否显示此日期
     arrDays: [],             //关于几号的信息
-    arrInfoEx: [],           //农历节假日等扩展信息
-    arrInfoExShow: [],       //处理后用于显示的扩展信息
-  },
-
-  //选择一天时显示的信息
-  detailData: {
-    curDay: "",         //detail中显示的日信息
-    curInfo1: "",
-    curInfo2: "",
-    curInfo3: "",
-  },
-
-  isWorkTurn: app.globalData.workTurnType.length != 0,
-  workTurnTypeName: app.globalData.workTurnTypeName,
-  workTurnType: app.globalData.workTurnType,
-  workTurnData: {
-    startWorkDate: {},
-    startWorkDateString: "",
-    arrWorkTurn: [],    //倒休信息
+    isCurentMonth: [],           //是否显示此日期
+    isSelectable: [],
+    isToday: [],
   },
 
   hot_list: [
@@ -98,49 +72,6 @@ var getOffset = function () {
   return offset;
 }
 
-//设置当前详细信息的索引，前台的详细信息会被更新
-var lastRefreshDetailDataIndex = 0;
-var refreshDetailData = function (index) {
-  if (index == null) {
-    index = lastRefreshDetailDataIndex
-  }
-  else {
-    lastRefreshDetailDataIndex = index;
-  }
-  var curEx = pageData.dateData.arrInfoEx[index];
-  if (!curEx)
-    return;
-  curDay = curEx.sDay;
-  pageData.detailData.curDay = curEx.sDay;
-  pageData.detailData.curInfo1 = "农历" + curEx.lunarMonth + "月" + curEx.lunarDay + " " + curEx.lunarFestival;
-  pageData.detailData.curInfo2 = curEx.cYear + curEx.lunarYear + "年 " + curEx.cMonth + "月 " + curEx.cDay + "日";
-  pageData.detailData.curInfo3 = pageData.workTurnData.arrWorkTurn[index];
-}
-
-var getWorkTurnDayInfo = function (d) {
-  var sd = pageData.workTurnData.startWorkDate;
-  var days = getOffDays(sd, d);
-  if (days < 0)
-    return "";
-
-  var index = days % pageData.workTurnType.length;
-  return pageData.workTurnType[index];
-}
-
-//刷新倒班视图
-var refreshWorkTurnData = function () {
-  if (app.globalData.workTurnType.length === 0)
-    return;
-
-  var offset = getOffset();
-  var offset2 = getDayCount(curYear, curMonth) + offset;
-  for (var i = 0; i < 42; ++i) {
-    if (i < offset || i >= offset2)
-      continue;
-    var d = new Date(curYear, curMonth, i - offset + 1);
-    pageData.workTurnData.arrWorkTurn[i] = getWorkTurnDayInfo(d);
-  }
-}
 
 //刷新全部数据
 var refreshPageData = function (year, month, day) {
@@ -153,41 +84,33 @@ var refreshPageData = function (year, month, day) {
   var offset = getOffset();
   var offset2 = getDayCount(curYear, curMonth) + offset;
   for (var i = 0; i < 42; ++i) {
-    pageData.dateData.arrIsShow[i] = i < offset || i >= offset2 ? false : true;
-    if (!pageData.dateData.arrIsShow[i])
-      continue;
-    pageData.dateData.arrDays[i] = i - offset + 1;
-    var d = new Date(year, month, i - offset + 1);
-    var dEx = calendarConverter.solar2lunar(d);
-    pageData.dateData.arrInfoEx[i] = dEx;
-    if ("" != dEx.lunarFestival) {
-      pageData.dateData.arrInfoExShow[i] = dEx.lunarFestival;
-    }
-    else if ("初一" === dEx.lunarDay) {
-      pageData.dateData.arrInfoExShow[i] = dEx.lunarMonth + "月";
-    }
-    else {
-      pageData.dateData.arrInfoExShow[i] = dEx.lunarDay;
+    pageData.dateData.isCurentMonth[i] = i < offset || i >= offset2 ? false : true;
+    // pageData.dateData.isSelectable[i] = (curYear < new Date().getFullYear()) || (curMonth < new Date().getMonth()) || (curDay < new Date().getDate()) ? false : true;
+    pageData.dateData.isToday[i] = curYear === new Date().getFullYear() && curMonth === new Date().getMonth() && (i - offset + 1 === new Date().getDate()) ? true : false;
+    if (i < offset){
+      if (curMonth === 0){
+        pageData.dateData.arrDays[i] = getDayCount(curYear - 1, 11) - offset + 1 + i;
+      } else {
+        pageData.dateData.arrDays[i] = getDayCount(curYear, curMonth - 1) - offset + 1 + i;
+      }
+    } else if (i >= offset2) {
+      pageData.dateData.arrDays[i] = i - offset2 + 1;
+    } else{
+      pageData.dateData.arrDays[i] = i - offset + 1;
     }
   }
-  refreshWorkTurnData();
-  refreshDetailData(offset + day - 1);
 };
 
-var refreshStartWorkDateString = function () {
-  var d = pageData.workTurnData.startWorkDate;
-  var month = d.getMonth();
-  var year = d.getFullYear();
-  var day = d.getDate();
-  pageData.workTurnData.startWorkDateString = year + "-" + (month + 1) + "-" + day;
-}
+// // // // // // // // // // // // // // 
+//
+//               初始化
+//
+// // // // // // // // // // // // // // 
 
 var curDate = new Date();
 var curMonth = curDate.getMonth();
 var curYear = curDate.getFullYear();
 var curDay = curDate.getDate();
-pageData.workTurnData.startWorkDate = app.globalData.startWorkDate;
-refreshStartWorkDateString();
 refreshPageData(curYear, curMonth, curDay);
 
 Page({
@@ -195,26 +118,15 @@ Page({
   onLoad: function () {
   },
   onShow: function () {
-    
-    pageData.isWorkTurn = app.globalData.workTurnType.length != 0;
-    pageData.workTurnTypeName = app.globalData.workTurnTypeName;
-    pageData.workTurnType = app.globalData.workTurnType;
-    refreshWorkTurnData();
-    refreshDetailData();
     this.setData({
       checkbox: 0,
-      isWorkTurn: pageData.isWorkTurn,
-      workTurnTypeName: pageData.workTurnTypeName,
-      workTurnType: pageData.workTurnType,
-      workTurnData: pageData.workTurnData,
-      detailData: pageData.detailData,
     })
   },
   onShareAppMessage: function () {
     return {
-      title: '倒班日历',
-      desc: '快速计算倒休情况，上班休假一目了然',
-      path: '/pages/calendar/calendar'
+      title: 'weMeet微历',
+      desc: '微历，让相聚更容易。',
+      path: '/pages/main/calendar/index'
     }
   },
 
@@ -226,8 +138,6 @@ Page({
     refreshPageData(curYear, curMonth, curDay);
     this.setData({
       dateData: pageData.dateData,
-      detailData: pageData.detailData,
-      workTurnData: pageData.workTurnData,
     })
   },
 
@@ -242,8 +152,6 @@ Page({
     refreshPageData(curYear, curMonth, 1);
     this.setData({
       dateData: pageData.dateData,
-      detailData: pageData.detailData,
-      workTurnData: pageData.workTurnData,
     })
   },
 
@@ -258,47 +166,18 @@ Page({
     refreshPageData(curYear, curMonth, 1);
     this.setData({
       dateData: pageData.dateData,
-      detailData: pageData.detailData,
-      workTurnData: pageData.workTurnData,
     })
   },
 
-  selectDay: function (e) {
-    refreshDetailData(e.currentTarget.dataset.dayIndex);
-    this.setData({
-      detailData: pageData.detailData,
-    })
-  },
 
   bindDateChange: function (e) {
     var arr = e.detail.value.split("-");
     refreshPageData(+arr[0], arr[1] - 1, +arr[2]);
     this.setData({
       dateData: pageData.dateData,
-      detailData: pageData.detailData,
-      workTurnData: pageData.workTurnData,
     })
   },
 
-  bindWorkTurnTypeTap: function (e) {
-    wx.navigateTo({
-      url: '../setting/setting'
-    });
-  },
-
-  bindWorkTurnDatePickerChange: function (e) {
-    pageData.workTurnData.startWorkDateString = e.detail.value;
-    var arr = e.detail.value.split("-");
-    pageData.workTurnData.startWorkDate = new Date(+arr[0], arr[1] - 1, +arr[2]);
-    //将开始上班日期信息存储起来
-    wx.setStorageSync("startWorkDate", pageData.workTurnData.startWorkDate);
-    refreshWorkTurnData();
-    refreshDetailData();
-    this.setData({
-      workTurnData: pageData.workTurnData,
-      detailData: pageData.detailData,
-    })
-  },
 
 
   // // // // // // // // // // // // // // 
